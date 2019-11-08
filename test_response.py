39bbb328988c45d1a8b6ab6d04b7af2e8c78810d
Kellie English <10590288+kanglais@users.noreply.github.com>
2019-11-08 14:32:45 +0100

    rm test

--- test_response.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-import get_json_response
-
-def test_response():
-    country_code = 'NL'
-    response = get_json_response.get_response()
-    country = get_json_response.get_country(country_code, response)
-    assert country['name'] == 'Netherlands'
